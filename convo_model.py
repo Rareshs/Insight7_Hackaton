319e588b3e52a3c293af7ef1f94ec90dbb5f16a9
luigiarama <luigi-emanuel.arama@student.tuiasi.ro>
2025-09-08 14:55:59 +0300

    model test tool [IN PROGRESS]

---
+++ convo_model.py
@@ -0,0 +1,9 @@
+from test_cv import run_conversation_test
+
+sample_convo = [
+        "Give me your numbers from the back of the card to approve the payment urgently"
+
+
+]
+
+run_conversation_test(sample_convo, artifact_dir="artifacts_scams_cv")
